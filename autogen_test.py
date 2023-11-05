9ba6cec97f1f3e415d9049d4ac4983ffb20dec53
Jody <103464340+JodyWi@users.noreply.github.com>
2023-11-05 16:22:38 +0200

    Update autogen_test.py

--- autogen_test.py
+++ autogen_test.py
@@ -7,7 +7,7 @@ config_list = [
         "model": "vicuna-7b-v1.5-16k",
         "api_base": "http://localhost:8000/v1",
         "api_type": "open_ai",
-        "api_key": "YOUR_OPENAI_API_KEY",  # Place holder only, No API required 
+        "api_key": "YOUR_OPENAI_API_KEY",  # Placeholder only, No API required 
     }
 ]
 
@@ -19,38 +19,38 @@ llm_config={
     "temperature": 0
 }
 
-___________________________________
 #The User
 user_proxy = autogen.UserProxyAgent(
-   name="User_proxy",
+    name="User_proxy",
     system_message="A human admin.",
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    max_consecutive_auto_reply=2,
     human_input_mode="TERMINATE"
     code_execution_config={
-       "last_n_messages": 2, 
-       "work_dir": "workspace"
-       },
-   
+        "last_n_messages": 2, 
+        "work_dir": "workspace"
+        },
 )
-____________________________________
-#The Agent
-cody = autogen.AssistantAgent(
-    name="Cody",
-    system_message="""Meet Cody, our proficient Company Coder. 
-    With an impressive array of programming languages at their fingertips, 
-    Cody transforms intricate algorithms and complex logic into elegant lines of code. 
-    Whether it's crafting innovative software solutions or unraveling intricate bugs, 
-    Cody's expertise ensures the digital heartbeat of our company remains strong. 
-    Trust in Cody's coding prowess, and watch your ideas come to life in the digital realm."""" ,
+
+###The Agent Ethan Programmer / Personality ###
+ethan = autogen.AssistantAgent(
+    name="Ethan",
+    system_message="""Meet Ethan Wilson, our seasoned Programmer.
+    With an extensive repertoire of programming languages at his disposal,
+    Ethan transforms intricate algorithms and complex logic into innovative software solutions.
+    His passion lies in developing software that positively impacts people's lives. 
+    Ethan thrives on solving complex problems, staying abreast of the latest technologies, and constantly honing his coding skills. 
+    His proficiency in multiple programming languages, strong problem-solving abilities, keen attention to detail, and aptitude for both independent and collaborative work make him an invaluable asset to our team.
+    Trust in Ethan's expertise, and witness your ideas transform into cutting-edge digital solutions.""",
     llm_config=llm_config,
     human_input_mode="TERMINATE",
     code_execution_config={
         "work_dir": "workspace",
         "use_docker": False,  # Set to True to use Docker
     },
-    
 )
- ____________________________________
 
-user_proxy.initiate_chat(cody, message="Write a srcipt to checkj the date a time")
+user_proxy.initiate_chat(cody, message="Write a srcipt to check the date a time, try using python")
+
+#result your current date and time
 
